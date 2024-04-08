fd98fc5e778f612603c20feddf8113020d0ada5a
SRS <rs.sku@yandex.ru>
2024-04-08 15:15:23 +0300

    main fixed

--- main.py
+++ main.py
@@ -8,17 +8,25 @@ engine = sqlalchemy.create_engine(DSN)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-inp = input('Введите имя издателя без пробела')
+inp = input('Введите имя или id издателя без пробела')
 
 
-def get_data_by_publisher(p_name: str) -> list:
+def get_data_by_publisher(p_name: str = None, p_id: int = None) -> list:
     data = []
-    query = (session.query(Book.title, Shop.name, Sale.price, Sale.date_sale)
-             .join(Publisher).join(Stock).join(Shop).
-             join(Sale).filter(Publisher.name == p_name))
-    for result in query.all():
-        data.append(result)
-    return data
+    if p_name:
+        query = (session.query(Book.title, Shop.name, Sale.price, Sale.date_sale)
+                 .join(Publisher).join(Stock).join(Shop).
+                 join(Sale).filter(Publisher.name == p_name))
+        for result in query.all():
+            data.append(result)
+        return data
+    if p_id:
+        query = (session.query(Book.title, Shop.name, Sale.price, Sale.date_sale)
+                 .join(Publisher).join(Stock).join(Shop).
+                 join(Sale).filter(Publisher.id == p_id))
+        for result in query.all():
+            data.append(result)
+        return data
 
 
 def print_data(data: list) -> None:
@@ -29,4 +37,8 @@ def print_data(data: list) -> None:
     print(result)
 
 
-print_data(get_data_by_publisher(inp))
+if inp.isdigit():
+    inp = int(inp)
+    print_data(get_data_by_publisher(p_id=inp))
+else:
+    print_data(get_data_by_publisher(p_name=inp))
